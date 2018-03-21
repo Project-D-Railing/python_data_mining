802e78ea024d18bb254937f1fb528376bb0255a9
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2018-03-21 17:07:32 +0100

    added database_connection.py to make building database connections easier.

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-import json
-import pymysql
-
+import database_connection as db_con
 import query_suite
 import math_util
 
 
-#read config file
-CONFIG_FILE_NAME = "app_config.json"
-
-config_file = open(CONFIG_FILE_NAME, "r", encoding='utf-8-sig', newline='\r\n')
-configuration = json.loads(config_file.read())
-config_file.close()
-
-
-#get config data
-dbcconfig = configuration["dbcconfig"]
-
-
-#connect to db
-try:
-    dbc = pymysql.connect(**dbcconfig)
-except pymysql.connector.Error as err:
-    print(err)
+dbc = db_con.connect_with_config(
+    config="app_config.json", property="dbcconfig")
 
     
 #setup query suite
