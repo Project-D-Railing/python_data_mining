b283dee1d53d9bbad19e13ff9f236966620fe3f3
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2018-03-23 03:06:09 +0100

    implemented more proccessing functions to calculate delays of trains.

--- eval_train_delay.py
+++ eval_train_delay.py
@@ -3,6 +3,7 @@ import query_suite_pandas
 import processing_utils as pu
 import pandas as pd
 
+#setup database connection
 dbc = db_con.connect_with_config(
     config="app_config.json", property="dbcconfig")
 
@@ -12,26 +13,77 @@ qsp.use_dbc(dbc)
 qsp.set_limit(5000)
 
 
-ttsids = qsp.get_ttsid_on_trip(dailytripid="-5016615278318514860", yymmddhhmm="1712011704")
+#get stops on trip
+#
+#ttsids = qsp.get_ttsid_on_trip(dailytripid="-5016615278318514860", yymmddhhmm="1712011704")
+ttsids = qsp.get_ttsid_on_trip(dailytripid="7983801099953614172", yymmddhhmm="1802031455")
 print(ttsids)
 
-delay_arrivals = pd.DataFrame();
-delay_depatures = pd.DataFrame();
+#dataframes for accumulating results
+staytime_scheduled_accum = pd.DataFrame()
+staytime_real_accum = pd.DataFrame()
+traveltime_real_accum = pd.DataFrame()
+traveltime_scheduled_accum = pd.DataFrame()
+delay_at_arrival_accum = pd.DataFrame()
+delay_at_depature_accum = pd.DataFrame()
+delay_by_staytime_accum = pd.DataFrame()
+delay_by_traveltime_accum = pd.DataFrame()
 
+#loop over trip
+train_stop_old = None
 for index, row in ttsids.iterrows():
     train_stop = qsp.get_tts_by_ttsid(row["ttsid"])
     station_name = qsp.get_stationname_by_evanr(train_stop["evanr"][0])
     print(train_stop)
     print(station_name)
-    delay_arrival = pu.calc_delay_at_arrival_df(train_stop)
-    delay_arrivals = delay_arrivals.append(delay_arrival)
-    delay_depature = pu.calc_delay_at_departure_df(train_stop)
-    delay_depatures = delay_depatures.append(delay_depature)
-    print("Delay at Arrival: ", delay_arrival)
-    print("Delay at Departure: ", delay_depature)
 
-print(delay_arrivals)
-print(delay_depatures)
+    staytime_scheduled = pu.calc_staytime_scheduled_df(train_stop)
+    staytime_scheduled_accum = staytime_scheduled_accum.append(staytime_scheduled)
+    staytime_real = pu.calc_staytime_real_df(train_stop)
+    staytime_real_accum = staytime_real_accum.append(staytime_real)
+    delay_at_arrival = pu.calc_delay_at_arrival_df(train_stop)
+    delay_at_arrival_accum = delay_at_arrival_accum.append(delay_at_arrival)
+    delay_at_depature = pu.calc_delay_at_departure_df(train_stop)
+    delay_at_depature_accum = delay_at_depature_accum.append(delay_at_depature)
+    delay_by_staytime = pu.calc_delay_by_staytime_df(train_stop)
+    delay_by_staytime_accum = delay_by_staytime_accum.append(delay_by_staytime)
+
+    traveltime_scheduled = None
+    traveltime_real = None
+    delay_by_traveltime = None
+    if train_stop_old is not None:
+        traveltime_scheduled = pu.calc_traveltime_scheduled_df(train_stop_old, train_stop)
+        traveltime_scheduled_accum = traveltime_scheduled_accum.append(traveltime_scheduled)
+        traveltime_real = pu.calc_traveltime_real_df(train_stop_old, train_stop)
+        traveltime_real_accum = traveltime_real_accum.append(traveltime_real)
+        delay_by_traveltime = pu.calc_delay_by_traveltime_df(train_stop_old, train_stop)
+        delay_by_traveltime_accum = delay_by_traveltime_accum.append(delay_by_traveltime)
+
+    #make step
+    train_stop_old = train_stop
+
+    print("Staytime Scheduled: ", staytime_scheduled)
+    print("Staytime Real: ", staytime_real)
+    if (train_stop_old is not None) and (traveltime_real is not None):
+        print("Traveltime Scheduled: ", traveltime_scheduled)
+        print("Traveltime Real: ", traveltime_real)
+    print("Delay at Arrival: ", delay_at_arrival)
+    print("Delay at Departure: ", delay_at_depature)
+    print("Delay by Staytime: ", delay_by_staytime)
+    if (train_stop_old is not None) and (traveltime_real is not None):
+        print("Delay by Traveltime: ", delay_by_traveltime)
+
+print(staytime_scheduled_accum)
+print(staytime_real_accum)
+print(traveltime_scheduled_accum)
+print(traveltime_real_accum)
+print(delay_at_arrival_accum)
+print(delay_at_depature_accum)
+print(delay_by_staytime_accum)
+print(delay_by_traveltime_accum)
+
+
+print(delay_by_traveltime_accum.reset_index().to_json())
 
 #clean up
 dbc.close()
--- processing_utils.py
+++ processing_utils.py
@@ -2,22 +2,22 @@ import datetime
 import pandas as pd
 
 
-def calc_traveltime_real_df(train_stop_from_df, train_stop_to_df):
+def calc_staytime_scheduled_df(train_stop_df):
     """
-    Calculates the real travel time between 'train_stop_from_df' and 'train_stop_to_df'.
-    :param train_stop_from_df: Pandas dataframe. Input for the train stop the train comes from.
-    :param train_stop_to_df: Pandas dataframe. Input for the train stop the train arrives at.
-    :return: Returns a pandas dataframe with columns 'traveltime', 'ttsid_from', 'ttsid_to'.
+    Calculates the scheduled staytime at train stop.
+    :param train_stop_df: Pandas dataframe. Input for the train stop the train stays at.
+    :return: Returns a pandas dataframe with columns 'staytime_scheduled', 'ttsid'.
     """
-    arzeitist_to = train_stop_to_df["arzeitist"][0]
-    dpzeitist_from = train_stop_from_df["dpzeitist"][0]
-    ttsid_from = train_stop_from_df["ttsid"][0]
-    ttsid_to = train_stop_to_df["ttsid"][0]
+    ttsid = train_stop_df["ttsid"][0]
+    arzeitsoll = train_stop_df["arzeitsoll"][0]
+    dpzeitsoll = train_stop_df["dpzeitsoll"][0]
 
-    traveltime = arzeitist_to - dpzeitist_from
-    result = pd.DataFrame(
-        data=[[traveltime, ttsid_from, ttsid_to]],
-        columns=["traveltime_real", "ttsid_from", "ttsid_to"])
+    if arzeitsoll is None or dpzeitsoll is None:
+        staytime = datetime.timedelta(0)
+    else:
+        staytime = dpzeitsoll - arzeitsoll
+
+    result = pd.DataFrame(data=[[staytime, ttsid]], columns=["staytime_scheduled", "ttsid"])
     return result
 
 
@@ -25,7 +25,7 @@ def calc_staytime_real_df(train_stop_df):
     """
     Calculates the real staytime at train stop.
     :param train_stop_df: Pandas dataframe. Input for the train stop the train stays at.
-    :return: Returns a pandas dataframe with columns 'staytime', 'ttsid'.
+    :return: Returns a pandas dataframe with columns 'staytime_real', 'ttsid'.
     """
     ttsid = train_stop_df["ttsid"][0]
     arzeitist = train_stop_df["arzeitist"][0]
@@ -37,6 +37,44 @@ def calc_staytime_real_df(train_stop_df):
         staytime = dpzeitist - arzeitist
 
     result = pd.DataFrame(data=[[staytime, ttsid]], columns=["staytime_real", "ttsid"])
+    return result
+
+
+def calc_traveltime_scheduled_df(train_stop_from_df, train_stop_to_df):
+    """
+    Calculates the scheduled travel time between 'train_stop_from_df' and 'train_stop_to_df'.
+    :param train_stop_from_df: Pandas dataframe. Input for the train stop the train comes from.
+    :param train_stop_to_df: Pandas dataframe. Input for the train stop the train arrives at.
+    :return: Returns a pandas dataframe with columns 'traveltime_scheduled', 'ttsid_from', 'ttsid_to'.
+    """
+    arzeitsoll_to = train_stop_to_df["arzeitsoll"][0]
+    dpzeitsoll_from = train_stop_from_df["dpzeitsoll"][0]
+    ttsid_from = train_stop_from_df["ttsid"][0]
+    ttsid_to = train_stop_to_df["ttsid"][0]
+
+    traveltime = arzeitsoll_to - dpzeitsoll_from
+    result = pd.DataFrame(
+        data=[[traveltime, ttsid_from, ttsid_to]],
+        columns=["traveltime_scheduled", "ttsid_from", "ttsid_to"])
+    return result
+
+
+def calc_traveltime_real_df(train_stop_from_df, train_stop_to_df):
+    """
+    Calculates the real travel time between 'train_stop_from_df' and 'train_stop_to_df'.
+    :param train_stop_from_df: Pandas dataframe. Input for the train stop the train comes from.
+    :param train_stop_to_df: Pandas dataframe. Input for the train stop the train arrives at.
+    :return: Returns a pandas dataframe with columns 'traveltime_real', 'ttsid_from', 'ttsid_to'.
+    """
+    arzeitist_to = train_stop_to_df["arzeitist"][0]
+    dpzeitist_from = train_stop_from_df["dpzeitist"][0]
+    ttsid_from = train_stop_from_df["ttsid"][0]
+    ttsid_to = train_stop_to_df["ttsid"][0]
+
+    traveltime = arzeitist_to - dpzeitist_from
+    result = pd.DataFrame(
+        data=[[traveltime, ttsid_from, ttsid_to]],
+        columns=["traveltime_real", "ttsid_from", "ttsid_to"])
     return result
 
 
@@ -78,17 +116,39 @@ def calc_delay_at_departure_df(train_stop_df):
     return result
 
 
-def calc_traveltime_scheduled_df(train_stop_from_df, train_stop_to_df):
-    pass
-
-
-def calc_staytime_scheduled_df(train_stop_df):
-    pass
-
-
-def calc_delay_by_traveltime_df(train_stop_from_df, train_stop_to_pf):
-    pass
-
-
 def calc_delay_by_staytime_df(train_stop_df):
-    pass
+    """
+    Calculates the delay that has been caused by the stay of the train.
+    Positive value means, that the stay time caused additional delay.
+    Negative value means, that the stay time decreased the delay.
+    :param train_stop_df: Pandas dataframe. Input for the train stop the train stays at.
+    :return: Returns a pandas dataframe with columns 'delay_by_staytime', 'ttsid'.
+    """
+    delay_at_departure = calc_delay_at_departure_df(train_stop_df)["delay_at_departure"][0]
+    delay_at_arrival = calc_delay_at_arrival_df(train_stop_df)["delay_at_arrival"][0]
+    ttsid = train_stop_df["ttsid"][0]
+
+    delay = delay_at_departure - delay_at_arrival
+    result = pd.DataFrame(data=[[delay, ttsid]], columns=["delay_by_staytime", "ttsid"])
+    return result
+
+
+def calc_delay_by_traveltime_df(train_stop_from_df, train_stop_to_df):
+    """
+    Calculates the delay that has been caused by the travel of the train.
+    Positive value means, that the travel time caused additional delay.
+    Negative value means, that the travel time decreased the delay.
+    :param train_stop_from_df: Pandas dataframe. Input for the train stop the train comes from.
+    :param train_stop_to_df: Pandas dataframe. Input for the train stop the train arrives at.
+    :return: Returns a pandas dataframe with columns 'delay_by_traveltime', 'ttsid_from', 'ttsid_to'.
+    """
+    traveltime_real = calc_traveltime_real_df(train_stop_from_df, train_stop_to_df)["traveltime_real"][0]
+    traveltime_scheduled = calc_traveltime_scheduled_df(train_stop_from_df, train_stop_to_df)["traveltime_scheduled"][0]
+    ttsid_from = train_stop_from_df["ttsid"][0]
+    ttsid_to = train_stop_to_df["ttsid"][0]
+
+    delay = traveltime_real - traveltime_scheduled
+    result = pd.DataFrame(
+        data=[[delay, ttsid_from, ttsid_to]],
+        columns=["delay_by_traveltime", "ttsid_from", "ttsid_to"])
+    return result
