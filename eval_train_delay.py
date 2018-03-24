5b1d8c36ae02b4408104a93c4ee2e34b3a12d910
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2018-03-24 03:49:15 +0100

    enhanced visualization

--- eval_train_delay.py
+++ eval_train_delay.py
@@ -28,7 +28,7 @@ staytime_real_accum = pd.DataFrame()
 traveltime_real_accum = pd.DataFrame()
 traveltime_scheduled_accum = pd.DataFrame()
 delay_at_arrival_accum = pd.DataFrame()
-delay_at_depature_accum = pd.DataFrame()
+delay_at_departure_accum = pd.DataFrame()
 delay_by_staytime_accum = pd.DataFrame()
 delay_by_traveltime_accum = pd.DataFrame()
 
@@ -50,8 +50,8 @@ for index, row in ttsids.iterrows():
     delay_at_arrival = pu.calc_delay_at_arrival_df(train_stop)
     delay_at_arrival_accum = delay_at_arrival_accum.append(delay_at_arrival, ignore_index=True)
 
-    delay_at_depature = pu.calc_delay_at_departure_df(train_stop)
-    delay_at_depature_accum = delay_at_depature_accum.append(delay_at_depature, ignore_index=True)
+    delay_at_departure = pu.calc_delay_at_departure_df(train_stop)
+    delay_at_departure_accum = delay_at_departure_accum.append(delay_at_departure, ignore_index=True)
 
     delay_by_staytime = pu.calc_delay_by_staytime_df(train_stop)
     delay_by_staytime_accum = delay_by_staytime_accum.append(delay_by_staytime, ignore_index=True)
@@ -80,7 +80,7 @@ for index, row in ttsids.iterrows():
         print("Traveltime Scheduled: ", traveltime_scheduled)
         print("Traveltime Real: ", traveltime_real)
     print("Delay at Arrival: ", delay_at_arrival)
-    print("Delay at Departure: ", delay_at_depature)
+    print("Delay at Departure: ", delay_at_departure)
     print("Delay by Staytime: ", delay_by_staytime)
     if (train_stop_old is not None) and (traveltime_real is not None):
         print("Delay by Traveltime: ", delay_by_traveltime)
@@ -92,18 +92,27 @@ print(staytime_real_accum)
 print(traveltime_scheduled_accum)
 print(traveltime_real_accum)
 print(delay_at_arrival_accum)
-print(delay_at_depature_accum)
+print(delay_at_departure_accum)
 print(delay_by_staytime_accum)
 print(delay_by_traveltime_accum)
 
 #visualize results
 delay_at_arrival_minutes = [t.total_seconds() / 60.0 for t in delay_at_arrival_accum["delay_at_arrival"]]
+delay_at_departure_minutes = [t.total_seconds() / 60.0 for t in delay_at_departure_accum["delay_at_departure"]]
+delay_by_staytime_minutes = [t.total_seconds() / 60.0 for t in delay_by_staytime_accum["delay_by_staytime"]]
+delay_by_traveltime_minutes = [t.total_seconds() / 60.0 for t in delay_by_traveltime_accum["delay_by_traveltime"]]
+
 plt.plot(delay_at_arrival_minutes)
-plt.ylabel('Verspätung bei Ankunft (Minuten)')
+plt.plot(delay_at_departure_minutes)
+plt.plot(delay_by_staytime_minutes)
+plt.plot(delay_by_traveltime_minutes)
+plt.legend(['Verspätung bei Ankuft', "Verspätung bei Abfahrt",
+            "Verspätung durch Haltezeit", "Verspätung durch Fahrtzeit"], loc='upper left')
+plt.ylabel('Zeit in Minuten')
 plt.xticks(np.arange(len(stationname_accum)), stationname_accum["stationname"], rotation=67)
 plt.grid(True)
 plt.title(train_stop_accum[0:1]["zugnummerfull"][0] + " am " + train_stop_accum[0:1]["datum"][0].strftime("%d.%m.%Y"))
-plt.subplots_adjust(bottom=0.4)
+plt.subplots_adjust(bottom=0.3)
 plt.show()
 
 
