f1ef72031f479b695772edce2f5f43e9a65ff11f
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2018-03-24 03:16:20 +0100

    added visualization

--- eval_train_delay.py
+++ eval_train_delay.py
@@ -1,7 +1,10 @@
 import database_connection as db_con
 import query_suite_pandas
 import processing_utils as pu
+
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 #setup database connection
 dbc = db_con.connect_with_config(
@@ -14,12 +17,12 @@ qsp.set_limit(5000)
 
 
 #get stops on trip
-#
-#ttsids = qsp.get_ttsid_on_trip(dailytripid="-5016615278318514860", yymmddhhmm="1712011704")
-ttsids = qsp.get_ttsid_on_trip(dailytripid="7983801099953614172", yymmddhhmm="1802031455")
+ttsids = qsp.get_ttsid_on_trip(dailytripid="-6843069272511463904", yymmddhhmm="1712011109")
 print(ttsids)
 
 #dataframes for accumulating results
+train_stop_accum = pd.DataFrame()
+stationname_accum = pd.DataFrame()
 staytime_scheduled_accum = pd.DataFrame()
 staytime_real_accum = pd.DataFrame()
 traveltime_real_accum = pd.DataFrame()
@@ -33,35 +36,44 @@ delay_by_traveltime_accum = pd.DataFrame()
 train_stop_old = None
 for index, row in ttsids.iterrows():
     train_stop = qsp.get_tts_by_ttsid(row["ttsid"])
-    station_name = qsp.get_stationname_by_evanr(train_stop["evanr"][0])
-    print(train_stop)
-    print(station_name)
+    train_stop_accum = train_stop_accum.append(train_stop, ignore_index=True)
+
+    stationname = qsp.get_stationname_by_evanr(train_stop["evanr"][0])
+    stationname_accum = stationname_accum.append(stationname, ignore_index=True)
 
     staytime_scheduled = pu.calc_staytime_scheduled_df(train_stop)
-    staytime_scheduled_accum = staytime_scheduled_accum.append(staytime_scheduled)
+    staytime_scheduled_accum = staytime_scheduled_accum.append(staytime_scheduled, ignore_index=True)
+
     staytime_real = pu.calc_staytime_real_df(train_stop)
-    staytime_real_accum = staytime_real_accum.append(staytime_real)
+    staytime_real_accum = staytime_real_accum.append(staytime_real, ignore_index=True)
+
     delay_at_arrival = pu.calc_delay_at_arrival_df(train_stop)
-    delay_at_arrival_accum = delay_at_arrival_accum.append(delay_at_arrival)
+    delay_at_arrival_accum = delay_at_arrival_accum.append(delay_at_arrival, ignore_index=True)
+
     delay_at_depature = pu.calc_delay_at_departure_df(train_stop)
-    delay_at_depature_accum = delay_at_depature_accum.append(delay_at_depature)
+    delay_at_depature_accum = delay_at_depature_accum.append(delay_at_depature, ignore_index=True)
+
     delay_by_staytime = pu.calc_delay_by_staytime_df(train_stop)
-    delay_by_staytime_accum = delay_by_staytime_accum.append(delay_by_staytime)
+    delay_by_staytime_accum = delay_by_staytime_accum.append(delay_by_staytime, ignore_index=True)
 
     traveltime_scheduled = None
     traveltime_real = None
     delay_by_traveltime = None
     if train_stop_old is not None:
         traveltime_scheduled = pu.calc_traveltime_scheduled_df(train_stop_old, train_stop)
-        traveltime_scheduled_accum = traveltime_scheduled_accum.append(traveltime_scheduled)
+        traveltime_scheduled_accum = traveltime_scheduled_accum.append(traveltime_scheduled, ignore_index=True)
+
         traveltime_real = pu.calc_traveltime_real_df(train_stop_old, train_stop)
-        traveltime_real_accum = traveltime_real_accum.append(traveltime_real)
+        traveltime_real_accum = traveltime_real_accum.append(traveltime_real, ignore_index=True)
+
         delay_by_traveltime = pu.calc_delay_by_traveltime_df(train_stop_old, train_stop)
-        delay_by_traveltime_accum = delay_by_traveltime_accum.append(delay_by_traveltime)
+        delay_by_traveltime_accum = delay_by_traveltime_accum.append(delay_by_traveltime, ignore_index=True)
 
     #make step
     train_stop_old = train_stop
 
+    print(train_stop)
+    print(stationname)
     print("Staytime Scheduled: ", staytime_scheduled)
     print("Staytime Real: ", staytime_real)
     if (train_stop_old is not None) and (traveltime_real is not None):
@@ -73,6 +85,8 @@ for index, row in ttsids.iterrows():
     if (train_stop_old is not None) and (traveltime_real is not None):
         print("Delay by Traveltime: ", delay_by_traveltime)
 
+print(train_stop_accum)
+print(stationname_accum)
 print(staytime_scheduled_accum)
 print(staytime_real_accum)
 print(traveltime_scheduled_accum)
@@ -81,6 +95,16 @@ print(delay_at_arrival_accum)
 print(delay_at_depature_accum)
 print(delay_by_staytime_accum)
 print(delay_by_traveltime_accum)
+
+#visualize results
+delay_at_arrival_minutes = [t.total_seconds() / 60.0 for t in delay_at_arrival_accum["delay_at_arrival"]]
+plt.plot(delay_at_arrival_minutes)
+plt.ylabel('Verspätung bei Ankunft (Minuten)')
+plt.xticks(np.arange(len(stationname_accum)), stationname_accum["stationname"], rotation=67)
+plt.grid(True)
+plt.title(train_stop_accum[0:1]["zugnummerfull"][0] + " am " + train_stop_accum[0:1]["datum"][0].strftime("%d.%m.%Y"))
+plt.subplots_adjust(bottom=0.4)
+plt.show()
 
 
 #clean up
